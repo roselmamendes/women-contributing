df1656647e8839a3f70d9058a9d8c89775ed1aa7
Roselma Mendes <rmendes@thoughtworks.com>
2015-01-11 17:14:20 -0300

    Put the 'run' and 'waitsFor' methods for treating asynchronous running. This was just for the first test in user_spec.js.

--- spec/user_spec.js
+++ spec/user_spec.js
@@ -1,12 +1,6 @@
 require ('../database');
 var User = require ('../models/user');
 
-beforeEach(function () {
-    new User({"created_at" : "2014-01-01"}).save();
-    new User({"created_at" : "2014-02-01"}).save();
-    new User({"created_at" : "2014-03-01"}).save();
-});
-
 afterEach(function () {
   User.remove({}, function() {
     });
@@ -14,13 +8,26 @@ afterEach(function () {
 
 describe("UserTime", function() {
   it("should get user count grouped by time", function (done) {
-    User.byTime(function (users) {
-      expect(users.length).toEqual(3);
-      expect(users).toEqual([ { _id: { year: 2014, month: 1 }, count: 1 },
-                              { _id: { year: 2014, month: 2 }, count: 1 },
-                              { _id: { year: 2014, month: 3 }, count: 1 } ]);
-      done();
+    var qty = 0;
+    runs(function (){
+      new User({"created_at" : "2014-01-01"}).save();
+      new User({"created_at" : "2014-02-01"}).save();
+      new User({"created_at" : "2014-03-01"}).save();
     });
+
+    waitsFor(function(){
+            User.byTime(function (users){
+                                        qty = users.length;
+                        });
+            return qty == 3;
+    },10000);
+
+    User.byTime(function (users) {
+              expect(users).toEqual([ { _id: { year: 2014, month: 1 }, count: 1 },
+                                      { _id: { year: 2014, month: 2 }, count: 1 },
+                                      { _id: { year: 2014, month: 3 }, count: 1 } ]);
+              done();
+                });
   });
 });
 
